5721716fb0606712331ec61a64ee89d0d77e2a87
Alex Korzh <alex@korzh.me>
2025-08-05 20:48:26 +0200

    Packages cleanup

--- fedora-install.sh
+++ fedora-install.sh
@@ -7,32 +7,24 @@ sudo dnf install -y v4l-util
 sudo dnf install -y guvcvie
 echo ">>>KeePassXC"
 sudo dnf install -y keepassxc
-echo ">>>1Password"
-sudo rpm --import https://downloads.1password.com/linux/keys/1password.asc
-sudo sh -c 'echo -e "[1password]\nname=1Password Stable Channel\nbaseurl=https://downloads.1password.com/linux/rpm/stable/\$basearch\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=\"https://downloads.1password.com/linux/keys/1password.asc\"" > /etc/yum.repos.d/1password.repo'
-sudo dnf install -y 1password
-# echo ">>>Kitty"
-# source ./kitty.sh
+echo ">>>Ghostty"
+sudo dnf copr enable -y scottames/ghostty
+sudo dnf install -y ghostty
 echo ">>>Fonts"
 sudo dnf install -y jetbrains-mono-fonts
 sudo dnf install -y adobe-source-code-pro-fonts
 source ./install-nerd-fonts JetBrainsMono
 source ./install-nerd-fonts SourceCodePro
+source ./aporetic.sh
 echo ">>>Shell"
 curl -sS https://starship.rs/install.sh | sh
 source ./zsh-install.sh
 echo ">>>Python"
 sudo dnf install -y python-pip
-curl https://pyenv.run | bash
-sudo dnf install -y nodejs
-sudo npm install -g pyright
+sudo dnf install -y uv
 echo ">>>emacs"
 sudo dnf install -y emacs
 echo ">>>Other langs"
 sudo dnf install -y ripgrep
 sudo dnf install -y clang
-echo ">>>VSCode"
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
-dnf check-update
-sudo dnf install -y code
+sudo dnf install -y fzf
